3b185619e1efdcf3735609e413132391390bd684
Huy Nhat Mai <nhathuylk0123@gmail.com>
2025-03-18 12:27:32 +1100

    Make SMS working

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,60 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Image, StyleSheet, View, Button, Alert } from 'react-native';
+import * as SMS from 'expo-sms';
 
 export default function HomeScreen() {
+  
+  function asktoSend() {
+    Alert.alert('SMS Send', 'Send: ' + id, [
+      {
+        text: 'Cancel',
+      },
+      {
+        text: 'ok', onPress: () => _handlePressButtonAsync()
+      }
+    ])
+  }
+
+  const id = ['0123456789', '9876543210'];
+  const message = 'My sample message';
+  async function _handlePressButtonAsync(){
+    const {result} = await SMS.sendSMSAsync(
+        id,
+        message,
+        {
+          attachments: {
+            uri: 'https://www.latrobe.edu.au/__data/assets/file/0010/796393/logo-white.svg',
+            mimeType: 'image/png',
+            filename: 'myfile.png'
+          },
+        }
+    );
+
+    if (result === 'sent') {
+      alert("Sent");
+    } else {
+      alert("Error: Check your balance or check phone");
+    }
+  } 
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <View style = {styles.header}>
+      <View style = {styles.containerRow}>
+        <Button 
+          title = "Send SMS"
+          onPress = {() => asktoSend()}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  header: {
+    padding: 40
+  },
+  containerRow: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+    gap: 8
   },
 });
